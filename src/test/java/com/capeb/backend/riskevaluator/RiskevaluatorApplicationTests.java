e5b46b1abf114cd91fdab5d302e656705769d019
1mohamedelasri <1mohamedelasri@gmail.com>
2022-01-09 18:11:39 +0100

    first commit

---
+++ src/test/java/com/capeb/backend/riskevaluator/RiskevaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capeb.backend.riskevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskevaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
