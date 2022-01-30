ddd7874bff6af1d22c0e3f80ea41d51563ec2165
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2022-01-30 02:52:34 +0100

    update Dev workflow

--- src/main/java/fr/capeb/backend/riskevaluator/util/CommandLineAppStartupRunner.java
+++ src/main/java/fr/capeb/backend/riskevaluator/util/CommandLineAppStartupRunner.java
@@ -16,20 +16,42 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Autowired
     public StatusRepository statusRepo;
 
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
+    @Value("${test.env.variable.port}")
+    private String port;
+    @Value("${test.env.variable.dbname}")
+    private String dbName;
+    @Value("${test.env.variable.user}")
     private String user;
-    @Value("${spring.datasource.password}")
+    @Value("${test.env.variable.pass}")
     private String pass;
+    @Value("${test.env.variable.host}")
+    private String host;
 
+
+    @Value("${spring.datasource.url}")
+    private String devUrl;
+    @Value("${spring.datasource.username}")
+    private String devUser;
+    @Value("${spring.datasource.password}")
+    private String devPass;
 
     @Override
     public void run(String...args) throws Exception {
-        log.info("MOHAMMED");
-        log.info("---"+url);
+        log.info("START TEST ENV VARIBALES ==================================== \n");
+        log.info("---"+host);
+        log.info("---"+port);
+        log.info("---"+dbName);
         log.info("---"+user);
         log.info("---"+pass);
+        log.info(" END TEST ENV VARIBALES ==================================== \n");
+
+
+
+        log.info("START DEV ENV VARIBALES ==================================== \n");
+        log.info("---"+devUrl);
+        log.info("---"+devUser);
+        log.info("---"+devPass);
+        log.info(" END DEV ENV VARIBALES ==================================== \n");
 
         Status status = Status.builder().id(1).ok("CAPEB ENV status OK").build();
         statusRepo.save(status);
