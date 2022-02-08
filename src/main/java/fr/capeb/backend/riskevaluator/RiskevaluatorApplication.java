ed912143e162ac9e7976f1a294051e3737191f9c
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2022-02-08 11:11:11 +0100

    Swagger UI Integration

--- src/main/java/fr/capeb/backend/riskevaluator/RiskevaluatorApplication.java
+++ src/main/java/fr/capeb/backend/riskevaluator/RiskevaluatorApplication.java
@@ -1,10 +1,12 @@
 package fr.capeb.backend.riskevaluator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class RiskevaluatorApplication {
 
 	public static void main(String[] args) {
