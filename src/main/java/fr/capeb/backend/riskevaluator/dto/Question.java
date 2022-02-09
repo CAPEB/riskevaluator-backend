5b4d4eced5f30d41ec01b9e6b1329f110feb7de5
ismail badzi <Badzi007@gmail.com>
2022-02-09 10:50:48 +0100

    add scoreMaxPossible

--- src/main/java/fr/capeb/backend/riskevaluator/dto/Question.java
+++ src/main/java/fr/capeb/backend/riskevaluator/dto/Question.java
@@ -20,8 +20,8 @@ public class Question {
     private QuestionType typeQuestion;
     @NotNull
     private String  libelleQuestion;
-    @NotNull
-    private String  aideQuestion;
+
+    private Integer scoreMaxPossible;
     @NotNull
     @JsonIgnoreProperties({"questionnaire","scoreEvaluations","questions","preconisationsCategorie"})
     private CategorieQuestion categorieQuestion;
--- src/main/java/fr/capeb/backend/riskevaluator/repository/MetierRepository.java
+++ src/main/java/fr/capeb/backend/riskevaluator/repository/MetierRepository.java
@@ -3,9 +3,17 @@ package fr.capeb.backend.riskevaluator.repository;
 import fr.capeb.backend.riskevaluator.model.MetierEntity;
 import fr.capeb.backend.riskevaluator.model.MetierQuestionEntityPK;
 import fr.capeb.backend.riskevaluator.model.PreconisationCategorieEntity;
+import fr.capeb.backend.riskevaluator.model.QuestionnaireEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MetierRepository extends JpaRepository<MetierEntity, Integer> {
+
+    @Query("select cnt from QuestionnaireEntity cnt where cnt.thematique = :tm")
+    Optional<QuestionnaireEntity> metierByQuestionnaireId(@Param("") Integer aQuestionnaireId);
 }
