b307655111d2b54dcef38597ff13e299b794b68b
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2022-03-11 13:15:51 +0100

    update ALLOWED CROSS ORIGIN

--- src/main/java/fr/capeb/backend/riskevaluator/security/WebSecurityConfig.java
+++ src/main/java/fr/capeb/backend/riskevaluator/security/WebSecurityConfig.java
@@ -85,6 +85,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 						"https://riskevaluator-frontend-dev2.herokuapp.com/",
 						"https://riskevaluator-frontend-dev.herokuapp.com/",
 						"https://capeb-riskeval.herokuapp.com/",
+						"http://capeb-riskeval.herokuapp.com/",
 						"http://localhost:4200/"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
 		config.setAllowCredentials(true);
